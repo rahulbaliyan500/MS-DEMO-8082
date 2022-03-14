34111a00b92260e345f5ce97dfaa7bd35a78200f
rahulbaliyan404 <rahulbaliyan404@gmail.com>
2022-03-14 22:14:26 +0530

    This is initial commit in 14-03-2022.

---
+++ MS-DEMO-8082/src/main/java/com/home/MsDemo8082Application.java
@@ -0,0 +1,13 @@
+package com.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDemo8082Application {
+//This is initial commit.
+	public static void main(String[] args) {
+		SpringApplication.run(MsDemo8082Application.class, args);
+	}
+
+}
